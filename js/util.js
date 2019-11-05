2862731529aea192d4d20a78097c69242bd1a27c
Minsu Han <mshanobject@gmail.com>
2019-11-05 15:13:03 +0900

    add document ready function to util.js

--- js/util.js
+++ js/util.js
@@ -1,13 +1,16 @@
-var btn = $("#nav_open");
-var target = $("nav > ul");
-btn.click(function(e){
-    e.preventDefault();
-    
-    if(target.is(".hide")) {
-        target.slideDown();
-        target.removeClass("hide");
-    } else {
-        target.slideUp();
-        target.addClass("hide");
-    }
+$(document).ready(function () {
+
+    var btn = $("#nav_open");
+    var target = $("nav > ul");
+    btn.click(function (e) {
+        e.preventDefault();
+
+        if (target.is(".hide")) {
+            target.slideDown();
+            target.removeClass("hide");
+        } else {
+            target.slideUp();
+            target.addClass("hide");
+        }
+    });
 });
