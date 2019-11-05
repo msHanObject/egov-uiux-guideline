95884533789be7f8a455ef7064810edbdd94e496
Minsu Han <mshanobject@gmail.com>
2019-11-05 19:06:10 +0900

    hover func add  to li

--- js/util.js
+++ js/util.js
@@ -1,16 +1,39 @@
 $(document).ready(function () {
-
     var btn = $("#nav_open");
-    var target = $("nav > ul");
-    btn.click(function (e) {
-        e.preventDefault();
-
-        if (target.is(".hide")) {
-            target.slideDown();
-            target.removeClass("hide");
+    var pt_list = $("nav>ul");
+    var navClickFunc = function(){
+        if (pt_list.is(".hide")) {
+            pt_list.slideDown();
+            pt_list.removeClass("hide");
         } else {
-            target.slideUp();
-            target.addClass("hide");
+            pt_list.slideUp();
+            pt_list.addClass("hide");
         }
+    };
+    btn.click(function(e){
+        navClickFunc();
     });
+    var pattern = $("nav>ul>li");
+    pattern.click(function(){
+       navClickFunc();
+    });
+    pattern.hover(
+        function(){
+            $(this).children("ul").slideDown();
+        },
+        function(){
+            $(this).children("ul").slideUp();
+        }
+    );
+    $("nav>ul>li>ul>li>a").focus(
+        function(){
+            console.log(this);
+            $(this).children("ul").slideDown();
+        }
+    );
+    $("nav>ul>li>ul>li>a").blur(
+        function(){
+            $(this).children("ul").slideUp();
+        }
+    );
 });
